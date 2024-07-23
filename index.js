3b9b72aef2756a86a874c9876fb07599a8565b66
ben mccarthy <bmccarthy90@gmail.com>
2024-07-23 16:29:36 -0400

    id displaying on page

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things!");
+//         console.log(error.message);
+//     });
+   
+function submitData (userName, userEmail) {
+
+    const userData = {
+        name: userName,
+        email: userEmail
+    }
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData),
+    };
+
+    console.log("confObj", configurationObject)
+    
+    fetch 
+    fetch("http://localhost:3000/users", configurationObject)
+        .then(response => response.json())
+        .then(serverData => {
+            console.log("srvDat", serverData.id)
+            
+            const body = document.querySelector('body')
+            console.log(body)
+            const para = document.createElement('p')
+            body.appendChild(para);
+            para.textContent = serverData.id;
+            
+            
+        })
+};
+
